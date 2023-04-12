03fdfee3f738b1bed664ca2c5830c50f1abf60b5
duova <61954674+duova@users.noreply.github.com>
2023-04-13 00:27:34 +0100

    Added a start button to main menu and TMP.

--- Assets/Scripts/ButtonFunctions.cs
+++ Assets/Scripts/ButtonFunctions.cs
@@ -3,13 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class ButtonFunctions : MonoBehaviour
 {
+    [SerializeField]
+    private int testSingleplayerIndex;
+    
     public void LoadScene(int index)
     {
         SceneManager.LoadScene(index);
     }
 
-    public void StartSingleplayerGame(int index)
+    public void StartSingleplayerGame()
     {
-        
+        GameManager.Instance.ClientSide = Side.Normal;
+        GameManager.Instance.GameType = GameType.Singleplayer;
+        LoadScene(testSingleplayerIndex);
     }
 }
