f43097a7f09e96e2bfb2b1f43e56b31aad9f98d7
kaiduova <k.chang0320211@arts.ac.uk>
2023-05-11 12:39:53 +0100

    Hotfix for AttackAction.

--- Assets/Scripts/Actions/AttackAction.cs
+++ Assets/Scripts/Actions/AttackAction.cs
@@ -14,6 +14,7 @@ namespace Actions
             var targetTileLocation = Piece.Actor.Side == Side.Normal ? Piece.Tile.location + relativeAttackCoordinate : Piece.Tile.location + MoveAction.InvertY(relativeAttackCoordinate);
             var targetTile = Board.Instance.Tiles.FirstOrDefault(tile => tile.location == targetTileLocation);
             if (targetTile == null) return;
+            if (targetTile.CurrentPiece == null) return;
             targetTile.CurrentPiece.Destroy();
             callback();
         }
